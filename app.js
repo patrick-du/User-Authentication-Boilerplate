a504844253e4f4591d700afc66ec6396ad437fab
Patrick Du <PDu@fgfbrands.com>
2019-04-25 11:17:55 -0400

    done login and logout

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
 const mongoose = require('mongoose');
-
+const flash = require('connect-flash');
+const session = require('express-session');
+const passport = require('passport');
 const app = express();
+
+// Passport Configs
+require('./config/passport')(passport);
 
 // DB Config
 const db = require('./config/keys').MongoURI;
@@ -19,6 +24,28 @@ app.set('view engine', 'ejs');
 
 // Bodyparser
 app.use(express.urlencoded({ extended: false }));
+
+// Express Session
+app.use(session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true,
+}));
+
+// Passport Middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Connect Flash
+app.use(flash());
+
+// Global Vars
+app.use((req, res, next) => {
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+    res.locals.error = req.flash('error');
+    next();
+});
 
 // Routes
 app.use('/', require('./routes/index'));
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
+const passport = require('passport');
 
 // User Model
 const User = require('../models/User');
@@ -69,6 +70,7 @@ router.post('/register', (req, res) => {
                             // Save user 
                             newUser.save()
                                 .then(user => {
+                                    req.flash('success_msg', 'You are now registered and can log in');
                                     res.redirect('/users/login');
                                 })
                                 .catch(err => console.log(err));
@@ -80,4 +82,19 @@ router.post('/register', (req, res) => {
     }
 });
 
+// Login Handle
+router.post('/login', (req, res, next) => {
+    passport.authenticate('local', {
+        successRedirect: '/dashboard',
+        failureRedirect: '/users/login',
+        failureFlash: true
+    })(req, res, next);
+})
+
+// Logout Handle
+router.get('/logout', (req, res) => {
+    req.logout();
+    req.flash('sucess_msg', 'You are logged out');
+    res.redirect('/users/login');
+})
 module.exports = router;
